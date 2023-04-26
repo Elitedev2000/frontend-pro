f1420c2c7f4324f21ed9608513d486909f1e8edf
elitedev <elitedev2000@gmail.com>
2023-04-26 23:03:20 +0500

    added some value and methods

---
+++ module-1/lesson-4/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let a = 12;
+let b = 13;
+
+// a += b;
+
+// a -= b;
+
+// a *= b;
+
+// a **= b;
+
+// a %= b;
+
+// a += "salom";
+
+// console.log(a);
+
+// console.log("false" * 1); // NaN
+
+// type conversion
+
+// console.log(typeof ("false" * 0));
+
+// console.log(typeof ("55" / 1));
+// console.log(10=='10');
+
+// --->>> ! is not - emas
+
+// console.log(10!=='10');
+
+// console.log(!true);
+
+// console.log(11 >= 10);
+// console.log(12 <= 9);
+
+// let num = 12 + "" + " "; // string tipi bo'ladi
+
+// console.log(typeof num);
+
+// let str = "javascript";
+
+// let number = Number(str); // tipini o'zgartirish uchun
+
+// console.log(typeof number);
+
+
+
+// ------- >>>> Truthy and Falthy
