f18e1557c5d21e9f66681b2a2bbbf48cddac945e
elitedev <elitedev2000@gmail.com>
2023-04-26 21:24:31 +0500

    added
    added all files

---
+++ module-1/lesson-3/script.js
@@ -0,0 +1,48 @@
+// document.write("Hello, world!");
+
+// console.log("Umidjon");
+// console.warn("Internet is unsafe");
+// console.error("Error 404");
+// console.info("Success");
+// console.table({ id: 1, name: "Umidjon", age: "25", job: "Coder" });
+
+// console.log(document.getElementById('text'));
+// document.getElementById('text').innerHTML = "<i>JavaScript</i>";
+
+/*
+
+katta izoh uchun
+
+*/
+
+// alert('Hello world!');
+
+// confirm('Sayt sizga yoqdimi?');
+
+// prompt("How are you?");
+
+/*
+
+console.log('1'+15-20);    // 95     
+console.log(11+'11');      // 1111  ---> + string bilan kelsa stringga o'tib qoladi 
+console.log(20+'20');      // 2020  ---> + string bilan kelsa stringga o'tib qoladi
+console.log('200'-50);     // 150   ---> number - boshqa amallar bilan
+console.log('200'/50);     // 4     ---> number - boshqa amallar bilan
+console.log(100*'3');      // 300   ---> number - boshqa amallar bilan
+console.log(100*'');       // 0     ---> truthy va falthy amallaridan 0 qiymat uchun falthy qaytaradi
+
+*/
+
+// ------------>>>>> DATA TYPES
+
+// const ism = "muhriddin"; // declare
+// ism = "husniddin"; // assign
+// const ism = "sardor"; // re declare
+
+// block scope - global ishlashni cheklaydi, var mustasno
+
+{
+  let ism = "bobur";
+}
+
+console.log(ism);
