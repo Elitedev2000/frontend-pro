bfd0e0e13d87029bfe9a7345c17874c60b2adfb9
elitedev <elitedev2000@gmail.com>
2023-04-30 18:11:18 +0500

    added ternary operators

--- module-1/lesson-5/script.js
+++ module-1/lesson-5/script.js
@@ -1,17 +1,47 @@
 "use strict";
 
-let a = 24;
+// let a = 24;
 // -->>> logical and - &&, logical or - ||
 
 // || - uchun bitta true kifoya
 // && - uchun false kifoya
 
-let ball = prompt("Balni kiriting:");
-let usd = prompt("Pul miqdorini kiriting:");
-let ielts = prompt("IELTS darajangizni kiriting:");
+// let ball = +prompt("Balni kiriting:");
+// let usd = +prompt("Pul miqdorini kiriting:");
+// let ielts = +prompt("IELTS darajangizni kiriting:");
 
-if (ball >= 160 || (ielts >= 5 && usd >= 5000)) {
-  console.log("Congratulations!");
-} else {
-  console.log("No successfull");
+// if (ball >= 160 || (ielts >= 5 && usd >= 5000)) {
+//   console.log("Congratulations!");
+// } else {
+//   console.log("No successfull");
+// }
+
+// ---->>>> Ternary Operators
+
+// 3 >= 2 < 6 ? console.log("6 katta") : console.log("3 katta");
+
+// let isBuy = false;
+
+// if (isBuy) {
+//   console.log("ok");
+// } else {
+//   console.log("no ok");
+// }
+
+// isBuy ? console.log("ha") : console.log("yoq");
+
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("not found");
 }
