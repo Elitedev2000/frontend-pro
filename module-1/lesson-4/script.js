2f788e8e50082c6673efbc53f939844209f9d085
elitedev <elitedev2000@gmail.com>
2023-04-30 12:20:13 +0500

    added code

--- module-1/lesson-4/script.js
+++ module-1/lesson-4/script.js
@@ -47,10 +47,68 @@
 
 // ------- >>>> Truthy and Falthy
 
-let a
+// Truthy ->> infinity
 
-if (11 > 10) {
-  console.log("ha");
+// Falthy ->> undefined, empty space, null, NaN, false, 0, -0, 0n
+
+// let a = "";
+
+// if (a) {
+//   console.log("ha");
+// } else {
+//   console.log("yoq");
+// }
+
+// console.log(12 * " ");
+
+// console.log(1 / 0);
+
+// ---->>>> Template literal
+
+// let str = "Java";
+
+// let str2 = "Script";
+
+// let str3 = str + str2;
+
+// console.log(` Welcome to ${str + str2} `);
+
+// --->>> Math
+
+let num = 12.34;
+
+// console.log(Math.floor(num)); // < yaxlitlaydi
+
+// console.log(Math.ceil(num)); // > yaxlitlaydi
+
+// console.log(Math.round(num)); // haqiqiy yaxlitlash
+
+// console.log(Math.abs(num)); // modulini chiqaradi
+
+// console.log(Math.max(2, 4, 6, 3, 66)); // eng kattasi
+
+// console.log(Math.min(3, 4, 1)); // eng kichigi
+
+// console.log(Math.pow(169, 1 / 2)); // ildiz olish uchun
+
+// console.log(Math.sqrt(25)); // kvadrat iltizni topish
+
+// console.log(Math.trunc(num)); // butun qismini oladi
+
+// console.log(Number(num.toFixed(1))); // nuqtadan keyin ko'rsatilgan qismgacha olib beradi
+
+// console.log(Math.random()*10);
+// console.log(Math.trunc(Math.random() * 100));
+
+// ---->>>>> COnditional operator " if else"
+
+let light = "yellow";
+let startedAt = 25;
+
+if (light === "green") {
+  console.log("Go!");
+} else if (light === "yellow") {
+  console.log("Attention");
 } else {
-  console.log("yoq");
+  console.log("STOP");
 }
