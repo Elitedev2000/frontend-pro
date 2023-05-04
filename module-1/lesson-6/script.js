ebe36317795e14fccf6f9652d9dc11a94fed86f0
elitedev <elitedev2000@gmail.com>
2023-05-04 11:32:25 +0500

    added object

--- module-1/lesson-6/script.js
+++ module-1/lesson-6/script.js
@@ -3,3 +3,49 @@
 // ----->>>> Object
 
 // literal object creation
+
+// let person = {
+//   isName: "Bobur",
+//   age: 20,
+//   isStudent: true,
+// };
+
+// console.log(person);
+
+// // new object()
+
+// const brand = new Object({ isNam: "Lexus" });
+
+// console.log(brand);
+
+// // object.create()
+
+// let newObj = Object.create({});
+
+// newObj.age = 24;
+// console.log(newObj);
+
+// // Object yaratish boshqa usullari  --- constructor, class, object.assign, JSON.stringify, JSON.parse
+
+// // dot nation
+
+// console.log(person.isName);
+
+// // bracket nation
+
+// console.log(person["age"]);
+
+let selectLang = prompt("Select language");
+
+let language = {
+  uz: {
+    Home: "Bosh sahifa",
+    About: "Biz haqimizda",
+  },
+  en: {
+    Home: "Home",
+    About: "About",
+  },
+};
+
+console.log(language[selectLang]);
