db9031c9730f9acc5504f14df1d578f4dfbe01cc
elitedev <elitedev2000@gmail.com>
2023-05-07 16:22:49 +0500

    added this method

--- module-1/lesson-7/script.js
+++ module-1/lesson-7/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+//// ---->>> this
+
+const device = {
+  name: "Iphone 14",
+  price: 1250,
+  color: "deep purple",
+  memory: { cpu: 16, cd: 256 },
+  camera: [12, 16, 48, 20],
+
+  about: function () {
+    return `Device name: ${this.name}`;
+  },
+};
+
+let iphone = device.about();
+
+console.log(iphone);
