ee5e5b3f7e2f7f4d7a5f2ec1b1e7753a64a206bf
elitedev <elitedev2000@gmail.com>
2023-05-16 12:45:00 +0500

    added click events

--- module-1/lesson-8/script.js
+++ module-1/lesson-8/script.js
@@ -17,15 +17,30 @@ let text = document.querySelector(".text"),
 
 // console.log(text.getAttribute("name"));
 
-function setInput() {
-  //   input.setAttribute("type", "password");
-  input.setAttribute("placeholer", "enter password");
-}
+// function setInput() {
+//   //   input.setAttribute("type", "password");
+//   input.setAttribute("placeholer", "enter password");
+//   console.log(input.value);
+// }
 
 // inline -> attribute
 
-btn.onclick = setInput;
+// btn.onclick = setInput;
 
 // btn.onclick = () => {
 //   setInput();
 // }; //------>>> callback function
+
+// console.log((text.style.backgroundColor = "aqua"));
+// console.log(text.style);
+// text.style.color = "red";
+
+text.style.cssText = "background-color: aqua; color:red; margin:30px";
+
+////// onClick, onChange, OnIn
+
+input.oninput = function () {
+  console.log(input.value);
+};
+
+console.log(text.style);
