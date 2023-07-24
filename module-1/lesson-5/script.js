c75245e8ebb4d200a420fe035faf74c7ae042103
Elitedev <elitedev2000@gmail.com>
2023-07-24 09:22:39 +0500

    added switch

--- module-1/lesson-5/script.js
+++ module-1/lesson-5/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let a = 24;
+/*
 
 // --- logical and &&, logical or ||
 
@@ -15,4 +15,39 @@ if ((ball >= 150) || (ielts >= 5 && usd >= 5000)) {
   console.log("Congratulation!");
 } else {
   console.log("No Successful");
+} */
+
+// ternary oparator
+
+// 1 > 2 ? console.log("ha") : console.log("yo'q");
+
+// 12 >= 13 ? console.log("ha") : console.log("ok");
+
+// let isBuy = true;
+
+// if (isBuy) {
+//   console.log("successfull");
+// } else {
+//   console.log("no successfull");
+// }
+
+// isBuy ? console.log('ha') : console.log('ok ');
+
+// 4 === 3 ? alert("success") : alert("no success");
+
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Friday");
+    break;
+
+  default:
+    console.log("not found");
 }
